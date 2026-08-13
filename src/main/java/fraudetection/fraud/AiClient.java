7003dc4c82c732a6f8022e980f74218b911e0fe8
Ricardosanhorta@gmail.com <Ricardosanhorta@gmail.com>
2026-08-13 20:29:24 +0100

    ADD exception to AiClient

--- src/main/java/fraudetection/fraud/AiClient.java
+++ src/main/java/fraudetection/fraud/AiClient.java
@@ -1,10 +1,12 @@
 package fraudetection.fraud;
 
+import fraudetection.exception.AiServiceUnavailableException;
 import fraudetection.fraud.dto.AiFraudPredictionRequestDTO;
 import fraudetection.fraud.dto.AiFraudPredictionResponseDTO;
 import fraudetection.transaction.Transaction;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClientException;
 
 @Component
 public class AiClient {
@@ -19,21 +21,25 @@ public class AiClient {
 
 
     public double getRiskScore(Transaction transaction){
+        try{
+            AiFraudPredictionRequestDTO request = new AiFraudPredictionRequestDTO(
+                    transaction.getAmount().doubleValue(),
+                    transaction.getTransactionAmountLevel().name(),
+                    transaction.getTransactionAmountLevel().name(),
+                    transaction.getTransactionType().name()
+            );
 
-        AiFraudPredictionRequestDTO request = new AiFraudPredictionRequestDTO(
-                transaction.getAmount().doubleValue(),
-                transaction.getTransactionAmountLevel().name(),
-                transaction.getTransactionAmountLevel().name(),
-                transaction.getTransactionType().name()
-        );
+            AiFraudPredictionResponseDTO response = restClient
+                    .post()
+                    .uri("/predict")
+                    .body(request)
+                    .retrieve()
+                    .body(AiFraudPredictionResponseDTO.class);
 
-        AiFraudPredictionResponseDTO response = restClient
-                .post()
-                .uri("/predict")
-                .body(request)
-                .retrieve()
-                .body(AiFraudPredictionResponseDTO.class);
-
-        return response.riskScore();
+            return response.riskScore();
+        }
+        catch(RestClientException e){
+            throw new AiServiceUnavailableException();
+        }
     }
 }
